0165ce354dab34d318c48d8a4ad99582a4da1155
Koichi Shiraishi <zchee.io@gmail.com>
2019-12-04 10:39:46 +0900

    doc: add package top-level synopsys

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package dap implements Debug Adapter Protocol (DAP) specification in Go.
+package dap
